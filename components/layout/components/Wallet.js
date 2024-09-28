69c4175345a402fb2ada2fb2f071759c4c07a1e7
Dhruvang Choudhari <dhruvangsc@gmail.com>
2024-09-28 23:01:33 +0530

    FIxed RPC error

--- components/layout/components/Wallet.js
+++ components/layout/components/Wallet.js
@@ -12,7 +12,7 @@ const networks = {
       symbol: "MATIC",
       decimals: 18,
     },
-    rpcUrls: ["https://rpc-amoy.polygon.technology/"],
+    rpcUrls: ["wss://polygon-amoy-bor-rpc.publicnode.com/"],
     blockExplorerUrls: ["https://amoy.polygonscan.com/"],
   },
 };
